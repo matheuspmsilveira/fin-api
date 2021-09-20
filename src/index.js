baf3d28dc19d0a9603a71b9c608dcec66b8ecbc7
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-20 12:33:46 -0300

    impl: middleware that checks for account exists

--- src/index.js
+++ src/index.js
@@ -7,12 +7,17 @@ app.use(express.json());
 
 const customers = [];
 
-/*
-  cpf - string
-  name - string
-  id - uuid
-  statement []
-*/
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) response.status(400).json({ error: 'Customer not found!' });
+
+  request.customer = customer;
+
+  return next();
+}
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
@@ -34,12 +39,11 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF); -> utiliza quando todos metodos abaixos
+// irão usufruir do middleware.
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) response.status(400).json({ error: 'Customer not found!' });
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
