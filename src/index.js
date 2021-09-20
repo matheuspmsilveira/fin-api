12e58864b0e4517a02cdd1a8cc33d660e9a06b3c
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-20 15:28:58 -0300

    impl: withdraw method

--- src/index.js
+++ src/index.js
@@ -19,6 +19,15 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statment) {
+  const balance = statment.reduce((acc, operation) => {
+    if (operation.type === 'credit') return acc + operation.amount;
+    return acc - operation.amount;
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -27,7 +36,7 @@ app.post('/account', (request, response) => {
   );
 
   if (customerAlreadyExists)
-    response.status(400).json({ error: 'Customer already exists!' });
+    return response.status(400).json({ error: 'Customer already exists!' });
 
   customers.push({
     cpf,
@@ -56,7 +65,27 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     description,
     amount,
     created_at: new Date(),
-    tyoe: 'credit',
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { amount } = request.body;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount)
+    return response.status(400).json({ error: 'Insufficient funds!' });
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
   };
 
   customer.statement.push(statementOperation);
